dc02c8bc052950d9af6892fb42a04d59a3bcf9f0
VatsalParikh0112 <vatsalparikh0112@gmai.com>
2025-09-16 11:08:28 +0530

    111

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'about/:aboutId',
+    renderMode: RenderMode.Server  // 👈 server-side rendered on request
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
