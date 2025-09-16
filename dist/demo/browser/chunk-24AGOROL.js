85569bf73e38bc59c37d2af530fa10ba58ee0655
VatsalParikh0112 <vatsalparikh0112@gmai.com>
2025-09-16 11:33:45 +0530

    123

--- dist/demo/browser/chunk-24AGOROL.js
+++
@@ -1,1 +0,0 @@
-import{Ba as o,Ya as n,Za as a,eb as p}from"./chunk-EPXFW6MZ.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-page-not-found"]],decls:2,vars:0,template:function(e,d){e&1&&(n(0,"p"),p(1,"page-not-found works!"),a())},encapsulation:2})};export{s as PageNotFound};
--- dist/demo/server/chunk-PG5CK6UO.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{$a as a,Ba as o,_a as n,gb as p}from"./chunk-QLXS2KWX.mjs";import"./chunk-X2SEQXRR.mjs";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-page-not-found"]],decls:2,vars:0,template:function(e,d){e&1&&(n(0,"p"),p(1,"page-not-found works!"),a())},encapsulation:2})};export{s as PageNotFound};
